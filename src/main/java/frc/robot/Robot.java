fba55116eb43f80e9ea9b37a7d3a370c5b5982d3
Adam Vander Pas <azvanderpas@gmail.com>
2025-01-04 18:10:35 -0600

    Fix AdvantageKit log code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,9 @@ import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import java.io.ByteArrayInputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.NoSuchElementException;
 import org.littletonrobotics.junction.*;
 import org.littletonrobotics.junction.networktables.NT4Publisher;
 import org.littletonrobotics.junction.wpilog.*;
@@ -29,13 +32,30 @@ public class Robot extends LoggedRobot {
       Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
       new PowerDistribution(1, ModuleType.kRev); // Enables power distribution logging
     } else {
-      setUseTiming(false); // Run as fast as possible
-      // Pull the replay log from AdvantageScope (or prompt the user)
-      String logPath = LogFileUtil.findReplayLog();
-      Logger.setReplaySource(new WPILOGReader(logPath)); // Read replay log
-      Logger.addDataReceiver(
-          new WPILOGWriter(
-              LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
+      try {
+        /*
+          findReplayLog() prompts the user for a log file path to replay, requiring input from the
+          user on the terminal.
+          This is a undesired interaction, so using setIn() to provide an empty line to the prompt.
+
+          findReplayLog() looks for the log file in AdvantageKit first, so this doesn't interrupt
+          our typical use case.
+        */
+        System.setIn(new ByteArrayInputStream("\n".getBytes("UTF-8")));
+        String logPath = LogFileUtil.findReplayLog();
+        if (logPath != null) {
+          setUseTiming(false); // Run as fast as possible
+
+          Logger.setReplaySource(new WPILOGReader(logPath)); // Read replay log
+          Logger.addDataReceiver(
+              new WPILOGWriter(
+                  LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
+        }
+      } catch (NoSuchElementException
+          | StringIndexOutOfBoundsException
+          | UnsupportedEncodingException ex) {
+        System.out.println("No log file found, simulating as normal. \n");
+      }
     }
 
     Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may
