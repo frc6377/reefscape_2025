3d724d77a8ff5938e214c6119b7e7807940ea345
Josh-Westbrook <116318801+Josh-Westbrook@users.noreply.github.com>
2025-01-14 20:57:05 -0600

    Butoon bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,9 @@
 
 package frc.robot;
 
-import static edu.wpi.first.units.Units.*;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
@@ -46,12 +48,14 @@ public class RobotContainer {
 
   private void configureBindings() {
     OI.getButton(OI.Driver.RBumper).and(OI.getButton(OI.Driver.LBumper)).onTrue(elevator.L0());
-    OI.getButton(OI.Driver.RBumper).and(OI.getButton(OI.Driver.A)).onTrue(elevator.L1());
-    OI.getButton(OI.Driver.RBumper).and(OI.getButton(OI.Driver.B)).onTrue(elevator.L2());
-    OI.getButton(OI.Driver.RBumper).and(OI.getButton(OI.Driver.X)).onTrue(elevator.L3());
+    OI.getButton(OI.Driver.RBumper).and(OI.getButton(OI.Driver.X)).onTrue(elevator.L1());
+    OI.getButton(OI.Driver.RBumper).and(OI.getButton(OI.Driver.A)).onTrue(elevator.L2());
+    OI.getButton(OI.Driver.RBumper).and(OI.getButton(OI.Driver.B)).onTrue(elevator.L3());
     OI.getButton(OI.Driver.RBumper).and(OI.getButton(OI.Driver.Y)).onTrue(elevator.L4());
-    OI.getButton(OI.Driver.B).and(OI.getButton(OI.Driver.RBumper).negate()).whileTrue(elevator.goUp());
-    OI.getButton(OI.Driver.A).and(OI.getButton(OI.Driver.RBumper).negate()).whileTrue(elevator.goDown());
+    OI.getButton(OI.Driver.RBumper).and(OI.getPOVButton(OI.Driver.POV0)).whileTrue(elevator.goUp());
+    OI.getButton(OI.Driver.RBumper)
+        .and(OI.getPOVButton(OI.Driver.POV180))
+        .whileTrue(elevator.goDown());
     OI.getButton(OI.Driver.Start).onTrue(elevator.zeroMotorEncoder());
 
     // Note that X is defined as forward according to WPILib convention,
@@ -74,19 +78,31 @@ public class RobotContainer {
 
     // Run SysId routines when holding back/start and X/Y.
     // Note that each routine should be run exactly once in a single log.
-    OI.getButton(OI.Driver.Back).and(OI.getButton(OI.Driver.Y)).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-    OI.getButton(OI.Driver.Back).and(OI.getButton(OI.Driver.X)).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-    OI.getButton(OI.Driver.Start).and(OI.getButton(OI.Driver.Y)).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-    OI.getButton(OI.Driver.Start).and(OI.getButton(OI.Driver.X)).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+    OI.getButton(OI.Driver.Back)
+        .and(OI.getButton(OI.Driver.Y))
+        .whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+    OI.getButton(OI.Driver.Back)
+        .and(OI.getButton(OI.Driver.X))
+        .whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+    OI.getButton(OI.Driver.Start)
+        .and(OI.getButton(OI.Driver.Y))
+        .whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+    OI.getButton(OI.Driver.Start)
+        .and(OI.getButton(OI.Driver.X))
+        .whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
     // reset the field-centric heading on left bumper press
     OI.getButton(OI.Driver.LBumper).onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
     drivetrain.registerTelemetry(logger::telemeterize);
-    
+
     // Set the intake rollers to the left and right triggers
-    OI.getPOVButton(OI.Driver.POV180).whileTrue(intake.IntakeCommand());
-    OI.getPOVButton(OI.Driver.POV0).whileTrue(intake.OuttakeCommand());
+    OI.getPOVButton(OI.Driver.POV180)
+        .and(OI.getButton(OI.Driver.RBumper).negate())
+        .whileTrue(intake.IntakeCommand());
+    OI.getPOVButton(OI.Driver.POV0)
+        .and(OI.getButton(OI.Driver.RBumper).negate())
+        .whileTrue(intake.OuttakeCommand());
   }
 
   public Command getAutonomousCommand() {
