503f95c8e64ae39dff80cdf529fa7e5efdeebb35
ModBoyex <79735432+ModBoyex@users.noreply.github.com>
2025-01-28 20:38:25 -0600

    New Template Project (#20)
    * add new template project
    
    * sim stuff
    
    * Remove old debug logging + unused classes
    
    * Add OI back
    
    * Use OI
    
    * remove deadband from drive command
    
    * add path
    
    * Add scoring Paths + scoring poses
    
    * add keyboard to OI
    
    * Add keyboard option for controls
    
    * update git ignore
    
    * fix spelling
    
    * remove file
    
    * better keyboard mapping toggle
    
    * remove log stop
    
    * new blank Pigeon 2 config

---
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -0,0 +1,61 @@
+// Copyright 2021-2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+package frc.robot.subsystems.vision;
+
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Inches;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+
+public class VisionConstants {
+  // AprilTag layout
+  public static AprilTagFieldLayout aprilTagLayout =
+      AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
+
+  // Camera names, must match names configured on coprocessor
+  public static String camera0Name = "limelight-lowcam";
+
+  // Robot to camera transforms
+  // (Not used by Limelight, configure in web UI instead)
+  public static Transform3d robotToCamera0 =
+      new Transform3d(
+          Inches.of(4.34645669),
+          Inches.of(9.99),
+          Inches.of(17.28346),
+          new Rotation3d(Degrees.of(21), Degrees.of(0), Degrees.zero()));
+
+  // Basic filtering thresholds
+  public static double maxAmbiguity = 0.3;
+  public static double maxZError = 0.75;
+
+  // Standard deviation baselines, for 1 meter distance and 1 tag
+  // (Adjusted automatically based on distance and # of tags)
+  public static double linearStdDevBaseline = 0.02; // Meters
+  public static double angularStdDevBaseline = 0.06; // Radians
+
+  // Standard deviation multipliers for each camera
+  // (Adjust to trust some cameras more than others)
+  public static double[] cameraStdDevFactors =
+      new double[] {
+        1, // Camera 0
+      };
+
+  // Multipliers to apply for MegaTag 2 observations
+  public static double linearStdDevMegatag2Factor = 0.5; // More stable than full 3D solve
+  public static double angularStdDevMegatag2Factor =
+      Double.POSITIVE_INFINITY; // No rotation data available
+}
