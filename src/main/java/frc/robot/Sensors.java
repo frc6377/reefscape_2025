dae3c1dfdee4ba707b56fd91aee055b866f38022
Arjun Mijar <arjunm@howdybots.org>
2025-02-08 12:44:43 -0600

    Add sim cases

--- src/main/java/frc/robot/Sensors.java
+++ src/main/java/frc/robot/Sensors.java
@@ -1,6 +1,12 @@
 package frc.robot;
 
 import static frc.robot.Constants.SensorIDs.*;
+
+import java.lang.Thread.State;
+
+import au.grapplerobotics.LaserCan;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import static edu.wpi.first.units.Units.Inches;
 
 import utilities.TOFSensorSimple;
@@ -15,17 +21,24 @@ public class Sensors {
   private TOFSensorSimple sensor4 = new TOFSensorSimple(kSensor4ID, Inches.of(1), TOFType.LASER_CAN);
   private TOFSensorSimple scorerSensor = new TOFSensorSimple(kScorerSensorID, Inches.of(1), TOFType.LASER_CAN);
 
+  public CoralEnum simState = CoralEnum.NO_CORAL;
+
   public Sensors() {
 
   }
 
   public CoralEnum getSensorState() {
+    if (Robot.isSimulation()) {
+      return simState;
+    }
+
     if (scorerSensor.isBeamBroke()) {
       return CoralEnum.IN_ELEVATOR;
     }
     int state = sensor2.isBeamBroke() ? 1 : 0;
     state += sensor3.isBeamBroke() ? 10 : 0;
     state += sensor4.isBeamBroke() ? 100 : 0;
+    SmartDashboard.putNumber("Laser Can State", state);
     switch (state) {
       case 000:
         return CoralEnum.NO_CORAL;
@@ -46,9 +59,12 @@ public class Sensors {
       default:
         return CoralEnum.NO_CORAL;
     }
+  }
 
+  public void setSimState(CoralEnum state) {
+    simState = state;
+  }
     // if (sensor2.isBeamBroke() && sensor3.isBeamBroke() && sensor4.isBeamBroke()) {
     //   return CoralEnum.NO_CORAL;
     // }
-  }
 }
