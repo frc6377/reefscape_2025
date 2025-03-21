504f9f8fbaabd64752e0ab51289e2c91fda3366f
Josh-Westbrook <116318801+Josh-Westbrook@users.noreply.github.com>
2025-03-20 21:30:32 -0500

    Fix climber Sim

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -403,11 +403,11 @@ public class RobotContainer {
     OI.getButton(OI.Operator.RTrigger).whileTrue(algeaRemover.downCommand());
 
     // Climber Buttons
-    // OI.getButton(OI.Operator.DPAD_UP)
-    //     .onTrue(climber.retract())
-    //     .toggleOnTrue(intake.movePivot(kPivotClimbingAngle));
-    // OI.getButton(OI.Operator.DPAD_LEFT).onTrue(climber.extendToCage());
-    // OI.getButton(OI.Operator.DPAD_DOWN).onTrue(climber.extendFully());
+    OI.getButton(OI.Operator.DPAD_UP)
+        .onTrue(climber.retract())
+        .toggleOnTrue(intake.movePivot(Constants.IntakeConstants.kPivotClimbingAngle));
+    OI.getButton(OI.Operator.DPAD_LEFT).onTrue(climber.extendToCage());
+    OI.getButton(OI.Operator.DPAD_DOWN).onTrue(climber.extendFully());
 
     // Reset gyro / odometry, Runnable
     final Runnable resetGyro =
